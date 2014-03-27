ef35fa5273cb03fcd0b16665a0ba491a9f233e9f
Rasmus Greve <rasmusgreve@gmail.com>
2014-03-27 07:55:43 +0100

    Implemented a data fetcher and serializer to csv. Downloaded ids 1-200000

--- BoardGameGeek/Boardgame.cs
+++ BoardGameGeek/Boardgame.cs
@@ -1,34 +1,122 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Xml;
 
 namespace BoardGameGeek
 {
     class Boardgame
     {
-
+        private static StreamWriter _fileWriter;
+        private static string[] _burstResult;
+        const int BurstSize = 10;
         public static void Main()
         {
+            /*
             var xd = new XmlDocument();
-            xd.Load("http://www.boardgamegeek.com/xmlapi2/thing?id=" + 25292 + "&stats=1&ratingcomments=1&pagesize=100&page=1");
-            Console.WriteLine(ParseXml(xd));
+            xd.Load("http://www.boardgamegeek.com/xmlapi2/thing?id=" + 0 + "&stats=1&ratingcomments=1&pagesize=100&page=1");
+            Boardgame b = ParseXml(xd);
+            Console.WriteLine(b);
+            Console.WriteLine("--------------------------");
+            if (CSVForID(0) == null)
+                Console.WriteLine("null");
+            else
+            {
+                Console.WriteLine(CSVForID(0));
+            }
             Console.ReadLine();
+            */
+            
+            _burstResult = new string[BurstSize];
+            var ts = new Thread[BurstSize];
+            _fileWriter = File.AppendText("data.csv");
+            var sw = new Stopwatch();
+            sw.Start();
+            for (int i = 1; i <= 200000; i+=BurstSize)
+            {
+                break; //@@@@@@@ Do nothing! The file has been made
+                //Start threads
+                for (int j = i; j < i + BurstSize; j++)
+                {
+                    ts[j-i] = new Thread(FetchDataAsync);
+                    ts[j - i].Start(new Tuple<int,int>(j,j-i));
+                }
+                //wait for completion
+                foreach (var thread in ts)
+                {
+                    thread.Join();
+                }
+                //write to file
+                foreach (var s in _burstResult)
+                {
+                    if (s == null) continue;
+                    _fileWriter.WriteLine(s);
+                }
+                _fileWriter.Flush();
+            }
+            _fileWriter.Close();
+            Console.WriteLine("All done! " + sw.ElapsedMilliseconds/1000 + "s");
+            Console.ReadLine();
+        }
+
+        public static void FetchDataAsync(Object o)
+        {
+            var v = (Tuple<int, int>)o;
+            int i = v.Item1, p = v.Item2;
+            string r = CSVForID(i);
+            _burstResult[p] = r;
+            if (r != null)
+            {   
+                Console.WriteLine("Id " + i + " done!");
+            }
+            else
+            {
+                Console.WriteLine("Id " + i + " skipped!");
+            }
+        }
+
+        public static string CSVForID(int id)
+        {
+            try
+            {
+                var xd = new XmlDocument();
+                xd.Load("http://www.boardgamegeek.com/xmlapi2/thing?id=" + id + "&stats=1&pagesize=100&page=1&type=boardgame");
+                var game = ParseXml(xd);
+                if (game == null) return null;
+                return game.ToCSV();
+            }
+            catch
+            {
+                return null;
+            }
         }
 
         public static Boardgame ParseXml(XmlDocument d)
         {
-            //if (!d.FirstChild.NextSibling.HasChildNodes) return null;
+            if (!d.FirstChild.NextSibling.HasChildNodes) return null;
             var result = new Boardgame();
 
             //Simple fields
+            result.Id = int.Parse(getSimpleValue(d, "/items/item", "id"));
             result.Name = getSimpleValue(d, "/items/item/name[@type='primary']", "value");
             result.YearPublished = int.Parse(getSimpleValue(d, "/items/item/yearpublished", "value"));
             result.MinPlayers = int.Parse(getSimpleValue(d, "/items/item/minplayers", "value"));
             result.MaxPlayers = int.Parse(getSimpleValue(d, "/items/item/maxplayers", "value"));
             result.PlayingTime = int.Parse(getSimpleValue(d, "/items/item/playingtime", "value"));
             result.MinAge = int.Parse(getSimpleValue(d, "/items/item/minage", "value"));
+
+            result.UsersRated = int.Parse(getSimpleValue(d, "/items/item/statistics/ratings/usersrated", "value"));
+            result.Average = double.Parse(getSimpleValue(d, "/items/item/statistics/ratings/average", "value").Replace(".",","));
+            result.StdDev = double.Parse(getSimpleValue(d, "/items/item/statistics/ratings/stddev", "value").Replace(".", ","));
+            result.Owned = int.Parse(getSimpleValue(d, "/items/item/statistics/ratings/owned", "value"));
+            result.Trading = int.Parse(getSimpleValue(d, "/items/item/statistics/ratings/trading", "value"));
+            result.Wanting = int.Parse(getSimpleValue(d, "/items/item/statistics/ratings/wanting", "value"));
+            result.Wishing = int.Parse(getSimpleValue(d, "/items/item/statistics/ratings/wishing", "value"));
+            result.NumComments = int.Parse(getSimpleValue(d, "/items/item/statistics/ratings/numcomments", "value"));
 
             //Player count votes
             var numPlayersNodes = d.SelectNodes("/items/item/poll[@name='suggested_numplayers']/results/result");
@@ -93,13 +181,21 @@ namespace BoardGameGeek
         }
 
 
-
+        public int Id { get; set; }
         public string Name { get; set; }
         public int YearPublished { get; set; }
         public int MinPlayers { get; set; }
         public int MaxPlayers { get; set; }
         public int PlayingTime { get; set; }
         public int MinAge { get; set; }
+        public int UsersRated { get; set; }
+        public double Average { get; set; }
+        public double StdDev { get; set; }
+        public int Owned { get; set; }
+        public int Trading { get; set; }
+        public int Wanting { get; set; }
+        public int Wishing { get; set; }
+        public int NumComments { get; set; }
         //numPlayers -> votes
         public Dictionary<int, int> NumPlayersBest { get; set; }
         public Dictionary<int, int> NumPlayersRecommended { get; set; }
@@ -121,16 +217,61 @@ namespace BoardGameGeek
             Families = new HashSet<int>();
         }
 
+        public string ToCSV()
+        {
+            return Id + ";" +
+                "\"" + Name + "\";" +
+                YearPublished + ";" +
+                MinPlayers + ";" +
+                MaxPlayers + ";" +
+                PlayingTime + ";" +
+                MinAge + ";" + 
+                UsersRated  + ";" + 
+                Average + ";" + 
+                StdDev + ";" + 
+                Owned + ";" + 
+                Trading + ";" + 
+                Wanting + ";" + 
+                Wishing + ";" +
+                NumComments + ";" + 
+                DictCSV(NumPlayersBest) + ";" +
+                DictCSV(NumPlayersRecommended) + ";" +
+                DictCSV(NumPlayersNotRecommended) + ";" +
+                DictCSV(SuggestedPlayerAge) + ";" +
+                SetCSV(Categories) + ";" +
+                SetCSV(Mechanics) + ";" +
+                SetCSV(Families)
+                ;
+        }
+
+        private static string DictCSV(Dictionary<int, int> dict)
+        {
+            return "{" + string.Join(";", dict.Select(v => v.Key + "=>" + v.Value)) + "}";
+        }
+
+        private static string SetCSV(ISet<int> set)
+        {
+            return "{" + string.Join(";", set) + "}";
+        }
 
         public override string ToString()
         {
             return "[ Board game " +
+                "\r\n\tId: " + Id +
                 "\r\n\tName: " + Name +
                 "\r\n\tYearPublished: " + YearPublished +
                 "\r\n\tMinPlayers: " + MinPlayers +
                 "\r\n\tMaxPlayers: " + MaxPlayers +
                 "\r\n\tPlayingTime: " + PlayingTime +
                 "\r\n\tMinAge: " + MinAge +
+                "\r\n\tUsersRated : " + UsersRated  +
+                "\r\n\tAverage: " + Average +
+                "\r\n\tStdDev: " + StdDev +
+                "\r\n\tOwned: " + Owned +
+                "\r\n\tTrading: " + Trading +
+                "\r\n\tWanting: " + Wanting +
+                "\r\n\tWishing: " + Wishing +
+                "\r\n\tNumComments: " + NumComments +
                 "\r\n\tNumPlayersBest: " + string.Join(", ", NumPlayersBest) +
                 "\r\n\tNumPlayersRecommended: " + string.Join(", ", NumPlayersRecommended) +
                 "\r\n\tNumPlayersNotRecommended: " + string.Join(", ", NumPlayersNotRecommended) +
